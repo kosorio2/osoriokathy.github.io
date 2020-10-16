1bd79d855b1a7fb5fc33d81a560967cd549c0554
kosorio2 <oso16001@byui.edu>
2020-10-15 21:44:17 -0400

    preston homepage update

--- lesson5/js/dates.js
+++ lesson5/js/dates.js
@@ -2,8 +2,15 @@ function toggleMenu() {
     console.log(document.getElementById("primaryNav").classList);
     document.getElementById("primaryNav").classList.toggle("hide");  
 }
+const day = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
-let Lm= document.lastModified; 
-let Y= new Date().getFullYear(); 
-document.getElementById("Modified").innerHTML = Lm; 
-document.getElementById("Year").innerHTML = Y; 
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+let date = new Date();
+let output = day[date.getDay()] + ", " + " " + date.getDate() + " " + months[date.getMonth()] + " " + date.getFullYear();
+
+document.getElementById('date').innerHTML = output; 
+
+if (date.getDay() == 5) {
+    document.getElementById("pop-up").style.display="block"; 
+}
