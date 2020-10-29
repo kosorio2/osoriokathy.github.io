ca77924d01b0b74375f88e831dcdb55d017e50b3
kosorio2 <oso16001@byui.edu>
2020-10-29 15:40:51 -0400

    lazy load

--- lesson7/js/lazy.js
+++ lesson7/js/lazy.js
@@ -1,10 +1,5 @@
 const imagesToLoad = document.querySelectorAll('img[data-src');
 
-const imgOptions = {
-    threshold: 0, 
-    rootMargin: "0px 0px 100px 0px"
-}
-
 const loadImages = (image) =>{
 image.setAttribute('src', image.getAttribute('data-src'));
 image.onLoad = () => {image.removeAttribute('data-source');
@@ -18,7 +13,7 @@ const imgObserver = new IntersectionObserver((items, imgObserver) => {
             loadImages(item.target); 
             imgObserver.unobserve(item.target); 
         }
-    }, imgOptions);
+    });
 }); 
 
 imagesToLoad.forEach((img) =>{
