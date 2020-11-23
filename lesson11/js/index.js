b3a521735f4e409217d5c10259fb208c384363f7
kosorio2 <oso16001@byui.edu>
2020-11-23 13:45:04 -0700

    lesson11

---
+++ lesson11/js/index.js
@@ -0,0 +1,66 @@
+const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
+const resultElement = document.getElementById('cards')
+
+fetch(requestURL)
+  .then(function (response) {
+    return response.json(); 
+  })
+  .then(function (jsonObject) {
+    console.log(jsonObject); 
+
+    const towns = jsonObject['towns'];
+    let card = document.createElement('section');
+
+
+    for ( let i = 0; i < towns.length; i++) {
+
+        if (towns[i].name == "Preston" || towns[i].name == "Fish Haven" || towns[i].name == "Soda Springs") {
+            let image = document.createElement('img')
+
+            let h2 = document.createElement('h2');
+            let div = document.createElement("div")
+            div.setAttribute('class', "division")
+            h2.setAttribute('class', "name")
+            let h5 = document.createElement('h5');
+            h5.setAttribute('class', "name")
+            let article = document.createElement('article');
+            article.setAttribute('class', "name")
+            let p = document.createElement('p');
+            p.setAttribute('class', "name")
+            let p2 = document.createElement('p');
+            p.setAttribute('class', "name")
+            let p3 = document.createElement('p');
+
+            image.setAttribute('src', 'images/' + towns[i].photo);
+            image.setAttribute('class', "photos" )
+            image.setAttribute('alt', 'Images depicting the towns')
+            image.apphend = towns[i].photo
+
+            h2.textContent = towns[i].name;
+            h5.textContent = towns[i].motto; 
+            p.textContent = "Year Founded: " + towns[i].yearFounded; 
+            p2.textContent = "Population: " + towns[i].currentPopulation;
+            p3.textContent = "Annual Rainfall: " + towns[i].averageRainfall;
+
+            
+    
+    
+            
+            div.appendChild(h2);
+            div.appendChild(h5);
+
+            div.appendChild(p)
+            div.appendChild(p2)
+            div.appendChild(p3)
+            article.appendChild(div)
+            article.appendChild(image)
+            
+    
+
+            card.appendChild(article); 
+        }
+
+    }
+    resultElement.appendChild(card); 
+
+}); 
